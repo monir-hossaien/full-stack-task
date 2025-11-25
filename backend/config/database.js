cc6d2fc536b9cf6a5cba121605ed691c59787f5f
Monir Hossain <monirhdigital@gmail.com>
2025-11-26 03:48:39 +0600

    mongodb uri correct

--- backend/config/database.js
+++ backend/config/database.js
@@ -14,10 +14,7 @@ const connectDB = async () => {
     }
 
     try {
-        const db = await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const db = await mongoose.connect(process.env.MONGODB_URI);
         isConnected = db.connections[0].readyState;
         console.log("MongoDB connected successfully");
     } catch (error) {
