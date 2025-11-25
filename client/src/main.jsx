f0beadda55683c5220e30143fbde851a5ec6ee41
Monir Hossain <monirhdigital@gmail.com>
2025-11-25 12:25:33 +0600

    issue solve

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,6 +11,7 @@ import {ToastContainer} from "react-toastify";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+        <App />
       <ToastContainer
           position="top-right"
           autoClose={5000}
